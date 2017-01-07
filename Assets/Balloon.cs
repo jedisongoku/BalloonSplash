61c4f2851d74ccc4631e49b9ccafd6a62546f0bf
jedisongoku <beratbekce@gmail.com>
2017-01-07 09:16:30 -0800

    Initial

---
+++ Assets/Balloon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Balloon : MonoBehaviour {
+
+    public float speed = 0.025f;
+    public int rotation = -1;
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(BalloonUp());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator BalloonUp()
+    {
+        transform.position = Vector3.Lerp(transform.position, transform.position + transform.up, speed);
+
+        yield return new WaitForSeconds(0);
+
+        StartCoroutine(BalloonUp());
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    public List<Color> backgroundColors = new List<Color>();
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ChangeBackgroundColor());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator ChangeBackgroundColor()
+    {
+
+        Camera.main.backgroundColor = backgroundColors[Random.Range(0, backgroundColors.Count - 1)];
+
+        yield return new WaitForSeconds(2);
+
+        StartCoroutine(ChangeBackgroundColor());
+    }
+}
